372c9a11ce5d9d9d5bbebafef59c419854b61cd1
Darrentan88 <112460520+Darrentan88@users.noreply.github.com>
2023-03-02 10:54:39 -0600

    Delete jss directory

--- jss/site.js
+++
@@ -1,9 +0,0 @@
-// jQuery 3.x-style ready event and locally scoped $
-jQuery(function($) {
-  $('html').removeClass('nojs');
-  $('html').addClass('hasjs');
-});
-var card = document.getElementbyId('cardfront');
-card.addEventListener('click',function(card){
-  card.target.classList.toggle('cardback');
-})
