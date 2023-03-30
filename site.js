6448c3bd4cfb46123dbe7db5b33dd3846b3e85a2
Darrentan88 <112460520+Darrentan88@users.noreply.github.com>
2023-03-30 17:49:34 -0500

    Update site.js

--- site.js
+++ site.js
@@ -1,7 +1,7 @@
 function changeImage() {
   var image = document.getElementById('myImage');
   if (image.src.match("thedevil.png")) {
-    image.src = "images/00001.png";
+    image.src = "images/120.png";
     }
   else {
     image.src = "images/thedevil.png";
